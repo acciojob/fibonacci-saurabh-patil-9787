86fd32e77faada1a9a4df1806fb35dfb97004290
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-03 16:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let output = document.getElementById('num')
 function fibonacci(num) {
 // your code here
+	let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if (num === 1) {
+        return num1;
+    } else if (num === 2) {
+        return num2;
+    } else {
+        for (let i = 3; i <= num; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
 }
 
+output.innerText = fibonacci(1)
 module.exports = fibonacci;
